098d63d8591d393cd5be4ee72a422b5cc4b4ff85
ekillham <ekillham@luc.edu>
2020-11-26 04:24:56 -0600

    Fixed Table Names / Cleanup
    Fixed all the table names after the DB was created and cleaned up unnecessary comments

--- Final Project/flaskDemo/models/models.py
+++ Final Project/flaskDemo/models/models.py
@@ -1,116 +1,83 @@
-#probably temporary since I will want to split this apart later for legibility
-from enum import unique
-from sqlalchemy.sql.schema import Column, ForeignKey
-from flaskDemo import db
-from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flaskDemo import db #add login_manager for login stuff below
+#from enum import unique
+#from sqlalchemy.sql.schema import Column, ForeignKey
+#from flask_sqlalchemy import SQLAlchemy
+#from flask_login import UserMixin
+
+
 
 db.Model.metadata.reflect(db.engine)
-# I think a lot of these need nullable
+
+# Created a seperate User table for login users that could be the system owner etc, not sure if we want to user that or tie to the fake users.
+
+#@login_manager.user_loader
+#def load_user(user_id):
+#    return Person.query.get(int(user_id))
+    
+#class User(db.Model, UserMixin):
+#    __table_args__ = {'extend_existing': True}
+#    user_id = db.Column(db.Integer, primary_key=True)
+#    username = db.Column(db.String(20), unique=True, nullable=False)
+#    email = db.Column(db.String(120), unique=True, nullable=False)
+#    password = db.Column(db.String(60), nullable=False)
+
+#    def __repr__(self):
+#        return f"User('{self.username}', '{self.email}')"
+
 class Person(db.Model):
-    __table__ = db.Model.metadata.tables['person']
-    # id            = db.Column(db.Integer, primary_key = True)
-    # first_name    = db.Column(db.String(25))
-    # last_name     = db.Column(db.String(30))
-    # email         = db.Column(db.String(255), unique = True, nullable = False)
-    # phone_numbers = db.relationship("person_phone_numbers") #ref to phone numbers
-    # person_type   = db.Column(db.String(50))
+    __table__ = db.Model.metadata.tables['Person']
 
-    # __mapper_args__ = { # this is what lets it have subtypes
-    #     'polymorphic_identity':'person',
-    #     'polymorphic_on':person_type
-    # }
+class Employee(db.Model):
+    __table__ = db.Model.metadata.tables['Employee']
 
-class Employee(Person):
-    __table__ = db.Model.metadata.tables['employee']
-    # __tablename__ = 'employee'
-    # id            = db.Column(db.Integer, db.ForeignKey('person.id'), primary_key = True) #all of sub types need this
-    # # possibly need managerID to add the manager relationship
-    # employee_type = db.Column(db.String(50)) # i wonder if a char can be used
+class Retiree(db.Model):
+    __table__ = db.Model.metadata.tables['Retiree']
 
-    # __mapper_args__ = {
-    #     'polymorphic_identity': 'employee',
-    #     'polymorphic_on'      : 'employee_type'
-    # }
+class Staff(db.Model):
+    __table__ = db.Model.metadata.tables['Staff']
 
-class Retiree(Employee):
-    __table__ = db.Model.metadata.tables['retiree']
-    # __tablename__      = 'retiree'
-    # id                 = db.Column(db.Integer, db.ForeignKey('employee.id'), primary_key = True) 
-    # retirement_date    = db.Column(db.Date)
-    # retirement_package = db.Column(db.String(255)) 
-    # __mapper_args__    = {
-    #     'polymorphic_identity': 'retiree'
-    # }
+class Faculty(db.Model):
+    __table__ = db.Model.metadata.tables['Faculty']
 
-class Staff(Employee):
-    __table__ = db.Model.metadata.tables['staff']
-
-class Faculty(Employee):
-    __table__ = db.Model.metadata.tables['faculty']
-
-
-
-class Student(Person):
-    __table__ = db.Model.metadata.tables['student']
-    # __tablename__ = 'student'
-    # id            = db.Column(db.Integer, db.ForeignKey('person.id'), primary_key = True)
-    # enrollment_status = db.column(db.String(20))
-    # credit_hours_total = db.column(db.Integer)
-    # student_type       = db.column(db.String(50)) #also might be able to be char
-
-    # __mapper_args__    = {
-    #     'polymorphic_identity': 'student',
-    #     # 'polymorphic_on'      : 'student_type'
-    # }
+class Student(db.Model):
+    __table__ = db.Model.metadata.tables['Student']
 
 class Undergrad(Student):
-    __table__ = db.Model.metadata.tables['undergrad']
+    __table__ = db.Model.metadata.tables['Undergrad']
 
 class Graduate(Student):
-    __table__ = db.Model.metadata.tables['graduate']
+    __table__ = db.Model.metadata.tables['Graduate']
 
 class Teaching_Assistant(Graduate):
-    __table__ = db.Model.metadata.tables['teaching_assistant']
+    __table__ = db.Model.metadata.tables['Teaching_Assistant']
 
 class Research_Assistant(Graduate):
-    __table__ = db.Model.metadata.tables['research_assistant']
+    __table__ = db.Model.metadata.tables['Research_Assistant']
 
 class Alumni(Student):
-    __table__ = db.Model.metadata.tables['alumni']
-
-# multivalued attribute
-class PersonPhoneNumber(db.Model):
-    __table__ = db.Model.metadata.tables['person_phone_number']
-    # __tablename__='person_phone_numbers'
-    # id = db.Column(db.Integer, db.ForeignKey('person.id'),primary_key = True )
-    # phone_number = db.Column(db.String(10))
+    __table__ = db.Model.metadata.tables['Alumni']
 
 class Department(db.Model):
-    __table__ = db.Model.metadata.tables['department']
+    __table__ = db.Model.metadata.tables['Department']
 
 class Office(db.Model):
-    __table__ = db.Model.metadata.tables['office']
+    __table__ = db.Model.metadata.tables['Office']
 
 class Building(db.Model):
-    __table__ = db.Model.metadata.tables['building']
+    __table__ = db.Model.metadata.tables['Building']
 
 class Campus(db.Model):
-    __table__ = db.Model.metadata.tables['campus']
+    __table__ = db.Model.metadata.tables['Campus']
 
 class Course(db.Model):
-    __table__ = db.Model.metadata.tables['course']
+    __table__ = db.Model.metadata.tables['Course']
 
-class Course_Teaching_Assistant(db.Model):
-    __table__ = db.Model.metadata.tables['course_teaching_assistant']
+class Enrolled_In(db.Model):
+    __table__ = db.Model.metadata.tables['Enrolled_In']
 
-class Undergrad_Enrolment(db.Model):
-    __table__ = db.Model.metadata.tables['undergrad_enrolment']
-
-class Graduate_Enrolment(db.Model):
-    __table__ = db.Model.metadata.tables['graduate_enrollment']
-
-class Course_Prerequisites(db.Model):
-    __table__ = db.Model.metadata.tables['course_prerequisites']
+class Prereqs(db.Model):
+    __table__ = db.Model.metadata.tables['Prereqs']
 
 #also just here so i can make sure I did my imports correctly 
 # class Student(db.Model):
